cedc44e2026e8924844f021b86ae77f22c6e5b6a
Suhyeon Kim <wisemuji@gmail.com>
2019-05-28 11:53:00 +0900

    #2 ProgressBar 동작 코드 구현

--- ProgressBar/Form1.cs
+++ ProgressBar/Form1.cs
@@ -12,9 +12,45 @@ namespace ProgressBar
 {
     public partial class Form1 : Form
     {
+        int Num = 0;    // 진행숫자
+        string OrgStr = ""; // 상태 : 저장 
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.OrgStr = this.lblStatus.Text;
+        }
+
+        private void BtnRun_Click(object sender, EventArgs e)
+        {
+            this.Timer.Enabled = true;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            this.Num++;
+            if (this.Num > 100)
+            {
+                this.Timer.Enabled = false;
+                return;
+            }
+            this.pbStatus.Value = Num;
+            this.lblStatus.Text = OrgStr + Num + "%";
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            this.Num = 0;
+            this.pbStatus.Value = Num;
+            this.lblStatus.Text = OrgStr;
+        }
+
+        private void BtnEnd_Click(object sender, EventArgs e)
+        {
+            this.Timer.Enabled = false;
         }
     }
 }
